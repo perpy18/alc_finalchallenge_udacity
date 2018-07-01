2d4a652499407d15cb2edd50eabb7751f1a42057
perpy18 <40118868+perpy18@users.noreply.github.com>
2018-07-01 02:42:18 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,15 +8,16 @@ $("input[type='submit']").on("click", function(event) {
 	event.preventDefault();  //this prevents the page from automatically reloading after "submit"
 makeGrid();
 });
-function clearTable() {$("tr").remove();
+function clearTable() {$("tr").remove();   //totally clears any pre-existing grid
+
 }
 
 function makeGrid() {
 clearTable();
+	
 let rows = $("#input_height").val(); //since Grid height equals number of rows
 let columns = $("#input_width").val();  //and Grid width equals number of columns
  
-//totally clears any pre-existing grid
 
 
 //set grid size by entering preferred values for grid height and width
@@ -25,9 +26,12 @@ for(let r  = 0; r < rows; r++) {
 	for (let c = 0; c < columns; c++) {
 		$("tr:last").append($("<td></td>"));
 	}
-}$("td").on("click", function() {
+}
+//To select cells and apply color to them
+$("td").on("click", function() {
 	var fillColour = $("#colorPicker").val();
 	//the use of "this" causes the action to be fired on selected cells only
+	//selecting an already-styled cell brings about removal of the style(in this cas, colour); this acts as an erase function to the user
 	if ($(this).attr("style")) {
 		$(this).removeAttr("style")}
 		else {$(this).attr("style", "background-color: " + fillColour)}
@@ -35,7 +39,6 @@ for(let r  = 0; r < rows; r++) {
 }
 
 
-//To select cells and apply color to them
 
 
 //}
